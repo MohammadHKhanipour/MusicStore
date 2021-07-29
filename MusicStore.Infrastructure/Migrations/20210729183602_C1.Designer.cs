c633b461b6b0bc822b70557ad22184f1eb7812dc
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-07-29 23:48:40 +0430

    Added C1 migration (ApplicationMessages)

--- MusicStore.Infrastructure/Migrations/20210729183602_C1.Designer.cs
+++ MusicStore.Infrastructure/Migrations/20210729183602_C1.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MusicStore.Infrastructure.Context;
 
 namespace MusicStore.Infrastructure.Migrations
 {
     [DbContext(typeof(MusicStoreDbContext))]
-    partial class MusicStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210729183602_C1")]
+    partial class C1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MusicStore.Infrastructure/Migrations/20210729183602_C1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicStore.Infrastructure.Migrations
+{
+    public partial class C1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Wallets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "UserNotifications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "ProductLinks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "ProductImages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "OrderDetails",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Bills",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "ApplicationUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationMessages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MessageType = table.Column<int>(type: "int", nullable: false),
+                    MessageCode = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationMessages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationMessages");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Wallets");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "UserNotifications");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "ProductLinks");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "ProductImages");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Bills");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "ApplicationUsers");
+        }
+    }
+}
