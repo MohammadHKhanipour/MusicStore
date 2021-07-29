b2a09dc7e6384557dc1559a486617daa407cc43d
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-07-29 18:01:07 +0430

    Added MessageType

---
+++ MusicStore.Framework/Enums/MessageType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicStore.Framework.Enums
+{
+    public enum MessageType
+    {
+        failed,
+        succeeded
+    }
+}
